6e61659f6db05414d1e3257be829c1477ff3b822
Vitthal-P <samrat.patil0202@gmail.com>
2017-01-15 12:54:08 +0530

    fixed Post model and added Post to admin page

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 
+from .models import Post
 # Register your models here.
-admin.register(posts)
+admin.site.register(Post)
--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=120)
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
