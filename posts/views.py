610f7dcfc06e04a7ce73d645a358818a02d1a7bc
Vitthal-P <samrat.patil0202@gmail.com>
2017-01-20 11:41:16 +0530

    implemented Django template inheritance in posts app

--- posts/views.py
+++ posts/views.py
@@ -32,7 +32,7 @@ def post_list(request):
         "title": "List",
         "object_list": queryset
     }
-    return render(request, 'index.html', context)
+    return render(request, 'post_list.html', context)
 
 def post_update(request, id):
     instance = get_object_or_404(Post, id=id)
@@ -42,7 +42,6 @@ def post_update(request, id):
         messages.success(request, "Post updated")
         instance.save()
         return HttpResponseRedirect(instance.get_absolute_url())
-
     context = {
         "title": "Edit Post",
         "form": form
