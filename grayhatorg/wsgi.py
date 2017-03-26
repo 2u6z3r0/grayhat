190d7e391ab17c7df1beeb3f58af966a13592faf
Vitthal-P <samrat.patil0202@gmail.com>
2017-03-26 09:23:30 +0530

    simple change

--- grayhatorg/wsgi.py
+++ grayhatorg/wsgi.py
@@ -8,14 +8,10 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grayhatorg.settings")
-
-application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
