1dc2f3b9ed4ab8ce0c938f1cf5e50fb5aeee9244
Vitthal-P <samrat.patil0202@gmail.com>
2017-04-03 12:13:53 +0530

    added logging

--- grayhatorg/settings/production.py
+++ grayhatorg/settings/production.py
@@ -51,3 +51,35 @@ MIDDLEWARE = [
 
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'posts': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
