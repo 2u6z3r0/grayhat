ec952aa87d7074d28540f3386d0601cd27e4a164
Vitthal-P <samrat.patil0202@gmail.com>
2017-05-30 18:45:34 -0700

    added whitenoise again to server static files

--- grayhatorg/settings/production.py
+++ grayhatorg/settings/production.py
@@ -51,35 +51,6 @@ MIDDLEWARE = [
 
 ]
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'mysite.log',
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers':['file'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-    },
-}
-
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'report.grayhat@gmail.com'
 EMAIL_HOST_PASSWORD = 'Maxy675689'
