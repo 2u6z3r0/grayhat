119e4ddfb05043ed1ee5c9178d01061d8809be07
Vitthal-P <samrat.patil0202@gmail.com>
2017-09-09 17:22:09 -0700

    associated users with view methods

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -5,12 +5,12 @@ from posts.models import Post
 class PostListSerializer(ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'title', 'slug', 'content', 'draft', 'publish']
+        fields = ['id', 'author', 'title', 'slug', 'content', 'draft', 'publish']
 
 class PostDetailSerializer(ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'title', 'slug', 'content', 'draft', 'publish']
+        fields = ['id', 'author', 'title', 'slug', 'content', 'draft', 'publish']
 
 class PostCreateUpdateSerializer(ModelSerializer):
     class Meta:
