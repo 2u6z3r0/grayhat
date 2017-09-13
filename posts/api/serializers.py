64862f24f774f1a6d709501931b21a014806afbc
Vitthal-P <samrat.patil0202@gmail.com>
2017-09-13 12:41:29 -0700

    Added url links in Post list view, on clicking it will take to post detail view of specific post

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,11 +1,16 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField
 
 from posts.models import Post
 
 class PostListSerializer(ModelSerializer):
+    url = HyperlinkedIdentityField(
+        lookup_field='slug',
+        view_name='posts-api:details'
+    )
+
     class Meta:
         model = Post
-        fields = ['id', 'author', 'title', 'slug', 'content', 'draft', 'publish']
+        fields = ['url', 'id', 'author', 'title', 'content', 'draft', 'publish']
 
 class PostDetailSerializer(ModelSerializer):
     class Meta:
