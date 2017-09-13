9e82e60b64c24f349b3589bcb79a39c7d6829471
Vitthal-P <samrat.patil0202@gmail.com>
2017-09-13 13:17:03 -0700

    added Serializer method fields author_name,image,html_content. Now Author name will be shown instead of Authod_id in views

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,4 +1,9 @@
-from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField
+from rest_framework.serializers import (
+    HyperlinkedIdentityField,
+    ModelSerializer,
+    SerializerMethodField
+    )
+
 
 from posts.models import Post
 
@@ -7,15 +12,35 @@ class PostListSerializer(ModelSerializer):
         lookup_field='slug',
         view_name='posts-api:details'
     )
-
+    author_name = SerializerMethodField()
     class Meta:
         model = Post
-        fields = ['url', 'id', 'author', 'title', 'content', 'draft', 'publish']
+        fields = ['url', 'id', 'author_name', 'title', 'content', 'draft', 'publish']
+
+    def get_author_name(self, obj):
+        return str(obj.author.get_full_name())
+
 
 class PostDetailSerializer(ModelSerializer):
+    author_name = SerializerMethodField()
+    image = SerializerMethodField()
+    html_content = SerializerMethodField()
     class Meta:
         model = Post
-        fields = ['id', 'author', 'title', 'slug', 'content', 'draft', 'publish']
+        fields = ['id', 'author_name', 'title', 'slug', 'content', 'html_content','draft', 'publish', 'image']
+
+    def get_author_name(self, obj):
+        return str(obj.author.get_full_name())
+
+    def get_image(self, obj):
+        try:
+            image = obj.image.url
+        except:
+            image= None
+        return image
+
+    def get_html_content(self, obj):
+        return obj.get_markdown()
 
 class PostCreateUpdateSerializer(ModelSerializer):
     class Meta:
