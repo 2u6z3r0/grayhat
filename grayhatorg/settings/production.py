c1fb787e676c2248561bb825bf1273de950c19a0
Vitthal-P <samrat.patil0202@gmail.com>
2017-09-23 20:45:03 +0530

    committed all changes after switching to macos

--- grayhatorg/settings/production.py
+++ grayhatorg/settings/production.py
@@ -1,5 +1,6 @@
 import os
 from django.conf import settings
+
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
@@ -10,7 +11,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-#disqus setup
+# disqus setup
 DISQUS_API_KEY = 'PJpRGLPMlp9nKc97zPtZAKJBPZT484KSTePTz6RRRGErGswul1RJryQ25Mijwimc'
 DISQUS_WEBSITE_SHORTNAME = 'grayhat-heroku'
 
@@ -21,11 +22,11 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
     }
- }
-
+}
 
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
